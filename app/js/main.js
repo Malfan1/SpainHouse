7eaeb108bb5d992a3a6a50618e978063bd9d413d
Malfan1 <artemdid911@gmail.com>
2023-07-30 23:30:12 +0300

    reload smth

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,13 @@
 const cli = document.querySelector(".js-burger");
-const navMob = document.querySelector(".js-navmob") 
+const navMob = document.querySelector(".js-navmob"); 
 
 document.addEventListener("click", handleScroll);
 
 function handleScroll({ target }) { 
     if (target.classList.contains("js-burger")) { 
         navMob.classList.add("block-menu");
-        
     } else if(target.classList.contains("js-close")) {
-        navMob.classList.remove("block-menu")
+        navMob.classList.remove("block-menu");
     } else {
         return;
     }
