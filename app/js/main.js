9072b225efa9a91d2c536c53d2b00f911ea104cd
Malfan1 <artemdid911@gmail.com>
2023-08-14 00:59:38 +0300

    aaas1

--- app/js/main.js
+++ app/js/main.js
@@ -41,3 +41,42 @@ $('.view__photos-big').slick({
     }
   }]
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector(".header-comp");
+    const main = document.querySelector(".main-sec__words");
+    const scrollItem = document.querySelectorAll(".scroll-item");
+
+    const scrollAnim = () => {
+        let windowCenter = (window.innerHeight / 1.05) + window.scrollY;
+        scrollItem.forEach(el => {
+            let scrollOffset = el.offsetTop + (el.offsetHeight / 1.05);
+            if (windowCenter >= scrollOffset) {
+                el.classList.add("anim-class");
+            } else {
+                el.classList.remove("anim-class");
+            }
+        })
+    };
+
+    const fixHeader = () => { 
+        let scrollTop = window.scrollY;
+        let mainCenter = main.offsetHeight / 2; 
+
+        if (scrollTop >= mainCenter) {
+            header.classList.add("fixed");
+            main.style.marginTop = `${header.offsetHeight}px`;
+        } else {
+            header.classList.remove("fixed");
+            main.style.marginTop = `0px`;
+        }
+    }
+
+    fixHeader();
+    scrollAnim();
+    window.addEventListener("scroll", () => { 
+        fixHeader();
+        scrollAnim();
+
+    })
+})
