d5ed83aed43cacf7a0d8c518d533a10f6e1e18d2
Malfan1 <artemdid911@gmail.com>
2023-10-03 01:09:20 +0300

    add modlebg

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,30 @@
 const cli = document.querySelector(".js-burger");
 const navMob = document.querySelector(".js-navmob"); 
+const bgModal = document.querySelector('.modal');
 
 document.addEventListener("click", handleScroll);
 
 function handleScroll({ target }) { 
     if (target.classList.contains("js-burger")) { 
         navMob.classList.add("block-menu");
-    } else if(target.classList.contains("js-close")) {
-        navMob.classList.remove("block-menu");
+        bgModal.classList.add('modal__show');
+    } else if(target.classList.contains("js-close") || target.classList.contains('modal')) {
+        closeMod();
     } else {
         return;
+    }
+}
+
+function closeMod() {
+    navMob.classList.remove("block-menu");
+    bgModal.classList.remove("modal__show");
+}
+
+document.addEventListener('keydown', escExit);
+
+function escExit(e) {
+    if (e.code === 'Escape' && navMob.classList.contains('block-menu') && bgModal.classList.contains('modal__show')) {
+        closeMod();
     }
 }
 
